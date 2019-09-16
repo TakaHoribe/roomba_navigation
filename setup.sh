778219ba41be5cfb987a078db18ab2247774592b
TakaHoribe <horibe.takamasa@gmail.com>
2019-09-16 22:26:16 +0900

    add roomba udev config

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# add udev rule for ydlidar
+sudo ./ros/src/ydlidar/startup/initenv.sh
+
+# add udev rule for roomba
+sudo touch /etc/udev/rules.d/roomba.rules
+echo KERNEL=="ttyUSB*", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", MODE:="0666", SYMLINK+="roomba" | sudo tee /etc/udev/rules.d/roomba.rules
+
