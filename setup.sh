13f2564ebe8d55b93949694b2f0004760441c9d4
TakaHoribe <horibe.takamasa@gmail.com>
2019-09-28 23:53:01 +0900

    fix setup.sh
    Signed-off-by: TakaHoribe <horibe.takamasa@gmail.com>

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ sudo ./ros/src/ydlidar/startup/initenv.sh
 
 # add udev rule for roomba
 sudo touch /etc/udev/rules.d/roomba.rules
-echo KERNEL=="ttyUSB*", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", MODE:="0666", SYMLINK+="roomba" | sudo tee /etc/udev/rules.d/roomba.rules
+echo 'KERNEL=="ttyUSB*", ATTRS{idVendor}=="0403", ATTRS{idProduct}=="6001", MODE:="0666", SYMLINK+="roomba"' | sudo tee /etc/udev/rules.d/roomba.rules
 
